911cc6bb1cb1e63b94911ddf2c84087e9fd72868
jakub-kilinski <roger00099@gmail.com>
2018-11-17 18:04:26 +0100

    Add handle of numbers of players and laps.

--- src/App.js
+++ src/App.js
@@ -13,12 +13,16 @@ class App extends Component {
 }
 
 
-class Stopwatch extends Component{
+class Stopwatch extends Component {
+    config = {
+        numbersOfPlayers: 4,
+        laps: 3
+    };
     KEYS = {
-        1: 49,
-        2: 50,
-        3: 51,
-        4: 52,
+        n1: 49,
+        n2: 50,
+        n3: 51,
+        n4: 52,
         q: 81,
         w: 87,
         e: 69,
@@ -26,16 +30,38 @@ class Stopwatch extends Component{
         x: 88,
         space: 32
     };
-    state = {
-        status: false,
-        runningTime: 0,
-        min: 0,
-        sec: 0,
-        ms: 0
+
+    constructor() {
+        super();
+        this.state = {
+            players: this.initializePlayersTime(),
+            isRunning: false,
+            runningTime: 0,
+            min: 0,
+            sec: 0,
+            ms: 0
+        }
+    }
+
+    componentDidMount() {
+        console.log('dupa');
+        document.body.addEventListener('keydown', this.recognizeKey);
     };
-    handleClick = () => {
+
+    initializePlayersTime() {
+        let players = [];
+        for(let i=0; i<this.config.numbersOfPlayers; i++){
+            players.push([]);
+            for(let j=0; j<this.config.laps; j++){
+                players[i].push(0);
+            }
+        }
+        return players;
+    };
+
+    runTimer = () => {
         this.setState(state => {
-            if (state.status) {
+            if (state.isRunning) {
                 clearInterval(this.timer);
             } else {
                 const startTime = Date.now() - this.state.runningTime;
@@ -49,21 +75,68 @@ class Stopwatch extends Component{
                     });
                 });
             }
-            return {status: !state.status};
+            return {isRunning: !state.isRunning};
         });
+    };
+    recognizeKey = (event) => {
+        let keyCode = event.keyCode;
+        if (keyCode === this.KEYS.space) {
+            console.log('space');
+            this.runTimer();
+        } else if (keyCode === this.KEYS.n1) {
+            console.log('1');
+        } else if (keyCode === this.KEYS.n2) {
+            console.log('2');
+        } else if (keyCode === this.KEYS.n3) {
+            console.log('3');
+        } else if (keyCode === this.KEYS.n4) {
+            console.log('4');
+        } else if (keyCode === this.KEYS.q) {
+            console.log('q');
+        } else if (keyCode === this.KEYS.w) {
+            console.log('w');
+        } else if (keyCode === this.KEYS.e) {
+            console.log('e');
+        } else if (keyCode === this.KEYS.r) {
+            console.log('r');
+        } else if (keyCode === this.KEYS.x) {
+            this.handleReset();
+        }
     };
     handleReset = () => {
         clearInterval(this.timer);
-        this.setState({runningTime: 0, min: 0, sec: 0, running: false});
+        this.setState({
+            runningTime: 0,
+            min: 0,
+            sec: 0,
+            ms: 0,
+            isRunning: false
+        });
     };
-    render(){
-        const {status, runningTime, min, sec, ms} = this.state;
+
+    renderPlayer = (data, id) => {
         return (
-            <div tabIndex={0} onKeyDown={this.handleClick}>
+            <div key={id}>
+                <span>{id}: </span>{data.map(this.renderTime)}
+            </div>
+        );
+    };
+
+    renderTime = (data, id) => {
+        return (
+            <span>{data ? data : "00:00:000"} </span>
+        );
+    };
+
+    render() {
+        const {runningTime, min, sec, ms} = this.state;
+        return (
+            <div>
                 <p>{runningTime}ms</p>
                 <p>{min}min {sec}s {ms}ms</p>
-                <button>{status ? 'Stop' : 'Start'}</button>
-                <button onClick={this.handleReset}>Reset</button>
+                <ul>
+                    {this.state.players.map(this.renderPlayer)}
+                </ul>
             </div>
         );
     }
