be8d8fdfb23d11c17632c846d90ca2f6483b6ede
jakub-kilinski <roger00099@gmail.com>
2018-11-27 22:13:57 +0100

    Set and display joker

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 class Driver {
     times = [];
     currentLap = 0;
-    wasJoker = false;
+    joker = false;
     startLapTime = 0;
 
     constructor(numberOfLaps) {
@@ -43,6 +43,14 @@ class Driver {
     getStartLapTime() {
         return this.startLapTime;
     }
+
+    setJoker() {
+        this.joker = true;
+    }
+
+    getJoker() {
+        return this.joker;
+    }
 }
 
 
@@ -61,7 +69,7 @@ class Stopwatch extends Component {
         e: 69,
         r: 82,
         x: 88,
-        space: 32
+        start: 13
     };
 
     constructor() {
@@ -118,10 +126,14 @@ class Stopwatch extends Component {
             : new Date(Date.now() - this.state.startTime);
         player.putLapTime(time);
     };
+    setJokerLap = (playerNumber) => {
+        let player = this.state.players[playerNumber];
+        player.setJoker();
+    };
     recognizeKey = (event) => {
         let keyCode = event.keyCode;
-        if (keyCode === this.KEYS.space) {
-            console.log('space');
+        if (keyCode === this.KEYS.start) {
+            console.log('start');
             this.runTimer();
         } else if (keyCode === this.KEYS.x) {
             this.handleReset();
@@ -136,38 +148,41 @@ class Stopwatch extends Component {
             } else if (keyCode === this.KEYS.n4) {
                 this.saveLapTime(3);
             } else if (keyCode === this.KEYS.q) {
-                console.log('q');
+                this.setJokerLap(0);
             } else if (keyCode === this.KEYS.w) {
-                console.log('w');
+                this.setJokerLap(1);
             } else if (keyCode === this.KEYS.e) {
-                console.log('e');
+                this.setJokerLap(2);
             } else if (keyCode === this.KEYS.r) {
-                console.log('r');
+                this.setJokerLap(3);
             }
         }
     };
     handleReset = () => {
-        clearInterval(this.timer);
-        this.setState({
-            min: 0,
-            sec: 0,
-            ms: 0,
-            isRunning: false,
-            startTime: 0,
-            runningTime: 0,
-            players: this.initializePlayersTime()
-        });
+        if(!this.state.isRunning){
+            clearInterval(this.timer);
+            this.setState({
+                min: 0,
+                sec: 0,
+                ms: 0,
+                isRunning: false,
+                startTime: 0,
+                runningTime: 0,
+                players: this.initializePlayersTime()
+            });
+        }
     };
 
     renderPlayer = (data, id) => {
         return (
             <div key={id}>
-                <span>{id}: </span>{data.times.map(this.renderTime)}
+                <span>{id}: </span><span>joker: {data.getJoker().toString()} </span>{data.times.map(this.renderTime)}
+                <hr/>
             </div>
         );
     };
 
-    renderPreetyTime(time) {
+    renderPrettyTime = (time)  => {
         return (
             <>
                 {('0' + time.getMinutes()).slice(-2)}:
@@ -178,7 +193,7 @@ class Stopwatch extends Component {
     };
     renderTime = (data, id) => {
         return (
-            <span key={'t' + id}>{data ? this.renderPreetyTime(data) : "00:00:000"} </span>
+            <span key={'t' + id}>{data ? this.renderPrettyTime(data) : "00:00:000"} </span>
         );
     };
 
