eeae057554dc236121939cc48f64db02d091debb
jakub-kilinski <roger00099@gmail.com>
2018-12-01 17:28:32 +0100

    Change file structure.

---
+++ src/Driver.js
@@ -0,0 +1,42 @@
+class Driver {
+    times = [];
+    currentLap = 0;
+    joker = false;
+    startLapTime = 0;
+
+    constructor(numberOfLaps) {
+        this.times = Driver.initTimes(numberOfLaps)
+    }
+
+    static initTimes(numberOfLaps) {
+        let times = [];
+        for (let j = 0; j < numberOfLaps; j++) {
+            times.push(0);
+        }
+        return times;
+    }
+
+    increaseLapCounter() {
+        this.currentLap++;
+    }
+
+    putLapTime(lapTime) {
+        this.times[this.currentLap] = lapTime;
+        this.startLapTime = Date.now();
+        this.increaseLapCounter();
+    }
+
+    getStartLapTime() {
+        return this.startLapTime;
+    }
+
+    setJoker() {
+        this.joker = true;
+    }
+
+    getJoker() {
+        return this.joker;
+    }
+}
+
+export default Driver;
